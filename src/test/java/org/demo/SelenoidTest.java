5739b9027608ac86ca5aa09232ac759ab90ec009
oshcherbina17 <110410608+oshcherbina17@users.noreply.github.com>
2023-06-09 18:18:04 +0300

    Update SelenoidTest.java

--- src/test/java/org/demo/SelenoidTest.java
+++ src/test/java/org/demo/SelenoidTest.java
@@ -7,8 +7,8 @@ import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -18,13 +18,10 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import java.io.File;
 import java.io.IOException;
-import org.apache.log4j.BasicConfigurator;  
-import org.apache.log4j.LogManager;  
-import org.apache.log4j.Logger;  
+
 
 public class SelenoidTest {
-   // private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-    private static final Logger logger = LogManager.getLogger(SelenoidTest.class);  
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public String url = "https://rozetka.com.ua/ua/";
 
@@ -55,14 +52,10 @@ public class SelenoidTest {
         wait.until(ExpectedConditions.titleIs(expectedTitle));
                 
         Assert.assertEquals(title, expectedTitle, "Title text not equals as expected");
-       // LOGGER.info("All ok.");
-        BasicConfigurator.configure();  
-        logger.info("Hello world");    
-        System.out.println("Test sout");
-        //Take the screenshot
+        LOGGER.info("All ok.");
+        
         File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         
-        //Copy the file to a location and use try catch block to handle exception
         try {
             FileUtils.copyFile(screenshot, new File("reports/screenshot.png"));
         } catch (IOException e) {
